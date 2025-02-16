81dbc5ad7db86c62ce86f75324826a691dfe049a
Arlan Viray <arlanviray@jonhucker.local>
2025-02-16 06:37:23 +0000

    ClassicGames - Setup layout for index page. Added a button to open the drawer.

--- ClassicGames/app/index.tsx
+++ ClassicGames/app/index.tsx
@@ -1,9 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import { DrawerActions } from "@react-navigation/native";
+import { useNavigation } from "expo-router";
 
 export default function index() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <ImageBackground
+        source={require("@/assets/images/classic-games.png")}
+        resizeMode="cover"
+        style={styles.bgImage}
+      />
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Play your game</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,5 +35,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    opacity: 0.3,
+  },
+  buttonContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#E4EFFF",
+    borderWidth: 4,
+    borderRadius: 18,
+    padding: 8,
+    marginBottom: 80,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "white",
+    backgroundColor: "black",
+    borderRadius: 14,
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
 });
