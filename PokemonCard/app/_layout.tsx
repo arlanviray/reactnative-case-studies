808a33dfb35ef5ea2c84aba0444ede9503d58c56
Arlan Viray <arlanviray@jonhucker.local>
2025-02-17 02:52:31 +0000

    PokemonCard - Setup to display landscape message orientation for the web (app currently set to portrait only on ios and android).

--- PokemonCard/app/_layout.tsx
+++ PokemonCard/app/_layout.tsx
@@ -1,25 +1,32 @@
 import { StatusBar, StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
+import { getOrientation, getLandscapeMessage } from "@/helpers/GetOrientation";
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.safeAreaView}>
-        <StatusBar barStyle={"dark-content"} />
-        <Stack>
-          <Stack.Screen
-            name="index"
-            options={{
-              title: "Popular Pokémon",
-              headerTitleAlign: "center",
-              headerShown: true,
-            }}
-          />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      {getOrientation() === "PORTRAIT" ? (
+        <SafeAreaProvider>
+          <SafeAreaView style={styles.safeAreaView}>
+            <StatusBar barStyle={"dark-content"} />
+            <Stack>
+              <Stack.Screen
+                name="index"
+                options={{
+                  title: "Popular Pokémon",
+                  headerTitleAlign: "center",
+                  headerShown: true,
+                }}
+              />
+              <Stack.Screen name="+not-found" />
+            </Stack>
+          </SafeAreaView>
+        </SafeAreaProvider>
+      ) : (
+        getLandscapeMessage()
+      )}
+    </>
   );
 }
 
